5d3a6900fd473e3adfbc5aaf90a999cf9952dd51
Matheus Marcelo <matheusmpmenezes@protonmail.com>
2023-08-08 21:42:06 -0300

    Create btn2.cpp

---
+++ btn2.cpp
@@ -0,0 +1,22 @@
+//
+int Tempo = 0;
+
+int Intensidade = 0;
+
+int i = 0;
+
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(2) == HIGH) {
+    digitalWrite(10, HIGH);
+  } else {
+    digitalWrite(10, LOW);
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
